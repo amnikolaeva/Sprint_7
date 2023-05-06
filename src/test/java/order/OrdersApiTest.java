8482fe2ecf16bd09cf176b580afa2437c3637250
Анна Николаева <amnik21@yandex.ru>
2023-05-06 20:41:53 +0300

    Добавила параметризацию в тест создания заказа

--- src/test/java/order/OrdersApiTest.java
+++ src/test/java/order/OrdersApiTest.java
@@ -4,14 +4,38 @@ import client.OrderClient;
 import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 
+@RunWith(Parameterized.class)
 public class OrdersApiTest {
 
+    private static final String BASE_PATH = "src/test/resources/";
+    private static final String ORDER_BLACK_PATH = BASE_PATH + "orderBlack.json";
+    private static final String ORDER_GREY_PATH = BASE_PATH + "orderGrey.json";
+    private static final String ORDER_BLACK_AND_GREY_PATH = BASE_PATH + "orderBlackAndGrey.json";
+    private static final String ORDER_WITHOUT_COLOR_PATH = BASE_PATH + "orderWithoutColor.json";
+
     private OrderClient orderClient;
+    private final File json;
+
+    public OrdersApiTest(File json) {
+        this.json = json;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                {new File(ORDER_BLACK_PATH)},
+                {new File(ORDER_GREY_PATH)},
+                {new File(ORDER_BLACK_AND_GREY_PATH)},
+                {new File(ORDER_WITHOUT_COLOR_PATH)}
+        };
+    }
 
     @Before
     public void setUp() {
@@ -21,7 +45,6 @@ public class OrdersApiTest {
 
     @Test
     public void orderCanBeCreatedWithBlackColor() {
-        File json = new File("src/test/resources/order.json");
         orderClient.orderCreate(json)
                 .statusCode(201)
                 .body("track", notNullValue());
